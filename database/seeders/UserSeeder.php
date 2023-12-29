d353e7cb9240f56def92b74913d35f2aefbb099c
GenRubio <burbianio@gmail.com>
2023-12-29 14:37:28 +0100

    WIP :: Update User Seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -15,12 +15,39 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         DB::table('api_users')->insert([
-            'id' => 1,
-            'web_socket_id' => '1',
-            'username' => 'admin',
-            'email' => 'root@root.com',
-            'password' => Hash::make('root'),
-            'content' => 'admin',
+            [
+                'id' => 1,
+                'web_socket_id' => '1',
+                'username' => 'user1',
+                'email' => 'user1@root.com',
+                'password' => Hash::make('user1'),
+                'content' => json_encode([
+                    'avatar_id' => 1,
+                    'avatar_color' => '64060E000000F6FAFFF6FAFFF6FAFFFFFFFFF6FAFF'
+                ]),
+            ],
+            [
+                'id' => 2,
+                'web_socket_id' => '2',
+                'username' => 'user2',
+                'email' => 'user2@root.com',
+                'password' => Hash::make('user2'),
+                'content' => json_encode([
+                    'avatar_id' => 2,
+                    'avatar_color' => '64060E000000F6FAFFF6FAFFF6FAFFFFFFFFF6FAFF'
+                ]),
+            ],
+            [
+                'id' => 3,
+                'web_socket_id' => '3',
+                'username' => 'user3',
+                'email' => 'user3@root.com',
+                'password' => Hash::make('user3'),
+                'content' => json_encode([
+                    'avatar_id' => 3,
+                    'avatar_color' => '64060E000000F6FAFFF6FAFFF6FAFFFFFFFFF6FAFF'
+                ]),
+            ],
         ]);
     }
 }
