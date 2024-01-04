4d512e744b016d1f3eb73fde23e7c643ec02c8a7
GenRubio <burbianio@gmail.com>
2024-01-04 09:20:44 +0100

    update

--- app/Http/Resources/UserResource.php
+++ app/Http/Resources/UserResource.php
@@ -9,6 +9,7 @@ class UserResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'username' => $this->username,
             'email' => $this->email,
             'content' => $this->content,
